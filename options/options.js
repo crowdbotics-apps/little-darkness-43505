9d81c13fc09d24c05a058926fb3f0030c5a94d50
Crowdbotics <team@crowdbotics.com>
2023-09-22 01:24:36 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "little-darkness-43505",
+  url: "https://little-darkness-43505.botics.co",
+  api: "https://little-darkness-43505.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
